d29f09be0882bba3cf4b691ebdcfcf6ddf5d6920
ev0-0ve <201424573+ev0-0ve@users.noreply.github.com>
2025-09-09 02:07:09 +0700

    Представление winForm и консольное приложение

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,64 @@
-﻿namespace ConsoleApp
+﻿using System;
+using Business_Logic;
+
+namespace ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Logic logic = new Logic();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Гильдия искателей приключений");
+                Console.WriteLine("Добро пожаловать!");
+                Console.WriteLine("\nВыберите действие:");
+                Console.WriteLine("1. Создать персонажа");
+                Console.WriteLine("2. Удалить персонажа");
+                Console.WriteLine("3. Показать всех персонажей");
+                Console.WriteLine("4. Изменить персонажа");
+                Console.WriteLine("5. Дополнительные функции");
+                Console.WriteLine("0. Выход");
+                Console.Write("\nВыбор: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine("Создание персонажа (пока заглушка)");
+                        // тут будет вызов logic.CreateCharacter(...)
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Удаление персонажа (пока заглушка)");
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Список персонажей (пока заглушка)");
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Изменение персонажа (пока заглушка)");
+                        break;
+
+                    case "5":
+                        Console.WriteLine("Бизнес-функция (пока заглушка)");
+                        break;
+
+                    case "0":
+                        return;
+
+                    default:
+                        Console.WriteLine("Неверный ввод, попробуйте снова");
+                        break;
+                }
+
+                Console.WriteLine("\nНажмите любую клавишу...");
+                Console.ReadKey();
+            }
         }
     }
 }
--- WindowsFormsApp1/MainForm.cs
+++ WindowsFormsApp1/MainForm.cs
@@ -1,20 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace WinFormApp
 {
     public partial class MainForm : Form
     {
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void buttonAddHero_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void buttonDeleteHero_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void buttonEditHero_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void buttonOpenMageForm_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void buttonOpenFighterForm_Click(object sender, System.EventArgs e)
+        {
+
         }
     }
 }
