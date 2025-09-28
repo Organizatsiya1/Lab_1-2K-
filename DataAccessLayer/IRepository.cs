84d3da0c709b208349b0dfbf89af7941d20f5963
Eskathorn <aster2006777@outlook.com>
2025-09-29 01:40:43 +0700

    Интерфес репозитория с CRUD и интерфейс доменного объекта

---
+++ DataAccessLayer/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogicModel;
+
+namespace DataAccessLayer
+{
+    public interface IRepository<T> where T : IDomainObject, new()
+    {
+        void Create(T obj);
+        IEnumerable<T> ReadAll();
+        T ReadByID(int id);
+        void Update(T obj);
+        void Delete(T obj);
+    }
+}
